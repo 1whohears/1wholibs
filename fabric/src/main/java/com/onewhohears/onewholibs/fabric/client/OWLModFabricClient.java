a1ca9882dddc43adebe91f21e56bd9265fdd0a97
1whohears <79810154+1whohears@users.noreply.github.com>
2025-08-26 21:10:12 -0500

    1.20.1 SPECIFIC | fixed OWLModFabricClient

--- fabric/src/main/java/com/onewhohears/onewholibs/fabric/client/OWLModFabricClient.java
+++ fabric/src/main/java/com/onewhohears/onewholibs/fabric/client/OWLModFabricClient.java
@@ -5,14 +5,14 @@ import com.onewhohears.onewholibs.client.model.obj.customanims.keyframe.Keyframe
 import com.onewhohears.onewholibs.client.renderer.RendererObjEntity;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
-import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 
 public final class OWLModFabricClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         EntityRendererRegistry.register(
-                Registry.ENTITY_TYPE.get(new ResourceLocation(OWLMod.MOD_ID, "test")),
+                BuiltInRegistries.ENTITY_TYPE.get(new ResourceLocation(OWLMod.MOD_ID, "test")),
                 context -> new RendererObjEntity<>(context,
                         new KeyframeAnimsEntityModel<>("ciws_test", "turret_test_anim")
                 )
