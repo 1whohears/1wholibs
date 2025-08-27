7c850d6dabf37df2f6cbfae32c3d8122dc66ac77
1whohears <79810154+1whohears@users.noreply.github.com>
2025-08-26 21:49:36 -0500

    1.20.1 SPECIFIC | fixed OWLModItems

--- common/src/main/java/com/onewhohears/onewholibs/init/OWLModItems.java
+++ common/src/main/java/com/onewhohears/onewholibs/init/OWLModItems.java
@@ -4,15 +4,15 @@ import com.onewhohears.onewholibs.OWLMod;
 import com.onewhohears.onewholibs.item.TestItem;
 import dev.architectury.registry.registries.DeferredRegister;
 import dev.architectury.registry.registries.RegistrySupplier;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
-import net.minecraft.core.Registry;
 
 import java.util.function.Supplier;
 
 public class OWLModItems {
 
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(OWLMod.MOD_ID, Registry.ITEM_REGISTRY);
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(OWLMod.MOD_ID, Registries.ITEM);
 
     public static final RegistrySupplier<Item> TEST_ITEM = register("test_obj_model_item", TestItem::create);
 
