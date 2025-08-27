5160123c684b483ad539a3c766a67a318858993f
1whohears <79810154+1whohears@users.noreply.github.com>
2025-08-26 21:51:05 -0500

    1.20.1 SPECIFIC | fixed OWLModEntities

--- common/src/main/java/com/onewhohears/onewholibs/init/OWLModEntities.java
+++ common/src/main/java/com/onewhohears/onewholibs/init/OWLModEntities.java
@@ -5,19 +5,19 @@ import com.onewhohears.onewholibs.OWLMod;
 import com.onewhohears.onewholibs.entity.TestEntity;
 import dev.architectury.registry.registries.DeferredRegister;
 import dev.architectury.registry.registries.RegistrySupplier;
-import net.minecraft.core.Registry;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityDimensions;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
-import net.minecraft.world.item.Item;
+import net.minecraft.world.flag.FeatureFlagSet;
 
 import java.util.function.Supplier;
 
 public class OWLModEntities {
 
     public static final DeferredRegister<EntityType<?>> ENTITY_TYPES = DeferredRegister.create(
-            OWLMod.MOD_ID, Registry.ENTITY_TYPE_REGISTRY);
+            OWLMod.MOD_ID, Registries.ENTITY_TYPE);
 
     public static RegistrySupplier<EntityType<TestEntity>> TEST = ENTITY_TYPES.register("test", testEntity());
 
@@ -31,7 +31,9 @@ public class OWLModEntities {
         return new EntityType<>(
                 factory, MobCategory.MISC,
                 true, true, false, true,
-                ImmutableSet.of(), size, 5, 3
+                ImmutableSet.of(),
+                size, 5, 3,
+                FeatureFlagSet.of()
         );
     }
 
