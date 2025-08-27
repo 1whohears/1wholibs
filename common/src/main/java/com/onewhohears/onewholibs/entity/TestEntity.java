05e8c0e71861310023fb03268b5e2ebf02a4e2a6
1whohears <79810154+1whohears@users.noreply.github.com>
2025-08-26 21:57:56 -0500

    1.20.1 SPECIFIC | fixed TestEntity

--- common/src/main/java/com/onewhohears/onewholibs/entity/TestEntity.java
+++ common/src/main/java/com/onewhohears/onewholibs/entity/TestEntity.java
@@ -1,12 +1,9 @@
 package com.onewhohears.onewholibs.entity;
 
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.network.protocol.Packet;
-import net.minecraft.network.protocol.game.ClientboundAddEntityPacket;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.level.Level;
-import org.jetbrains.annotations.NotNull;
 
 public class TestEntity extends Entity {
 
@@ -27,11 +24,5 @@ public class TestEntity extends Entity {
     @Override
     protected void addAdditionalSaveData(CompoundTag compoundTag) {
 
-    }
-
-    @Override
-    public @NotNull Packet<?> getAddEntityPacket() {
-        // FIXME - NetworkHooks is a Forge API; replace this with a sided-impl if this doesn't work as intended
-        return new ClientboundAddEntityPacket(this);
     }
 }
