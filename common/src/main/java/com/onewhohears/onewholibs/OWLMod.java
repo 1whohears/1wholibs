a9843634483647f5c9b0a5ac44a02abdd0e6e1fc
1whohears <79810154+1whohears@users.noreply.github.com>
2025-09-03 13:50:45 -0500

    registering all the JsonPresetReloadListeners happens during the SETUP event now so other mods can register their listeners in time if they hook into the register event during init

--- common/src/main/java/com/onewhohears/onewholibs/OWLMod.java
+++ common/src/main/java/com/onewhohears/onewholibs/OWLMod.java
@@ -1,7 +1,5 @@
 package com.onewhohears.onewholibs;
 
-import com.google.common.base.Suppliers;
-import com.mojang.logging.LogUtils;
 import com.onewhohears.onewholibs.client.event.OWLClientEventHandlers;
 import com.onewhohears.onewholibs.client.model.obj.ObjEntityModels;
 import com.onewhohears.onewholibs.client.model.obj.customanims.keyframe.KFAnimPlayers;
@@ -13,18 +11,10 @@ import com.onewhohears.onewholibs.common.event.OWLEvents;
 import com.onewhohears.onewholibs.common.event.OWLReloadListener;
 import com.onewhohears.onewholibs.common.event.ServerHolder;
 import com.onewhohears.onewholibs.common.network.OWLPacketHandler;
-import com.onewhohears.onewholibs.data.jsonpreset.JsonPresetReloadListener;
 import com.onewhohears.onewholibs.init.OWLModEntities;
 import com.onewhohears.onewholibs.init.OWLModItems;
 import dev.architectury.platform.Platform;
-import dev.architectury.registry.ReloadListenerRegistry;
-import dev.architectury.registry.registries.RegistrarManager;
 import dev.architectury.utils.Env;
-import net.minecraft.server.packs.PackType;
-import org.slf4j.Logger;
-
-import java.util.List;
-import java.util.function.Supplier;
 
 /**
  * @author <a href="https://github.com/1whohears">1whohears</a>
@@ -32,9 +22,6 @@ import java.util.function.Supplier;
  */
 public class OWLMod {
     public static final String MOD_ID = "onewholibs";
-    private static final Logger LOGGER = LogUtils.getLogger();
-
-    public static final Supplier<RegistrarManager> REGISTRIES = Suppliers.memoize(() -> RegistrarManager.get(MOD_ID));
 
     public static void init() {
         OWLPacketHandler.init();
@@ -47,11 +34,6 @@ public class OWLMod {
         OWLCommonEventHandlers.init();
         OWLReloadListener.register();
         OWLEvents.registerPresetTypesEvent();
-        List<JsonPresetReloadListener<?>> listeners = OWLEvents.getJsonPresetReloadListeners();
-        listeners.forEach(listener -> {
-            listener.registerDefaultPresetTypes();
-            ReloadListenerRegistry.register(PackType.SERVER_DATA, listener);
-        });
     }
 
     public static void clientInit() {
