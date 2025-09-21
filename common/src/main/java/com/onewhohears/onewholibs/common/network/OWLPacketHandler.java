5de7e3d819667d0ed81b4670aeb3b1c3fe455370
1whohears <79810154+1whohears@users.noreply.github.com>
2025-09-21 17:19:40 -0500

    using a ClientBoundAddJsonPresetEntityPacket to send additional spawn data to the client

--- common/src/main/java/com/onewhohears/onewholibs/common/network/OWLPacketHandler.java
+++ common/src/main/java/com/onewhohears/onewholibs/common/network/OWLPacketHandler.java
@@ -1,13 +1,13 @@
 package com.onewhohears.onewholibs.common.network;
 
 import com.onewhohears.onewholibs.OWLMod;
-import com.onewhohears.onewholibs.common.network.toclient.*;
+import com.onewhohears.onewholibs.common.network.toclient.ToClientCanSeePos;
+import com.onewhohears.onewholibs.common.network.toclient.ToClientDataPackSync;
+import com.onewhohears.onewholibs.common.network.toclient.ToClientSyncGameRules;
+import com.onewhohears.onewholibs.common.network.toclient.ToClientTestPreset;
 import com.onewhohears.onewholibs.common.network.toserver.ToServerCanSeePos;
-import com.onewhohears.onewholibs.entity.JsonPresetEntityHolder;
 import dev.architectury.networking.simple.MessageType;
 import dev.architectury.networking.simple.SimpleNetworkManager;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.level.ChunkPos;
 
 public final class OWLPacketHandler {
 
@@ -19,8 +19,6 @@ public final class OWLPacketHandler {
             "data_pack_sync", ToClientDataPackSync::new);
     public static final MessageType S2C_SYNC_GAME_RULES = INSTANCE.registerS2C(
             "sync_game_rules", ToClientSyncGameRules::new);
-    public static final MessageType S2C_SYNC_PRESET_ENTITY = INSTANCE.registerS2C(
-            "sync_preset_entity", ToClientSyncPresetEntity::new);
     public static final MessageType S2C_TEST_PRESET = INSTANCE.registerS2C(
             "test_preset", ToClientTestPreset::new);
     public static final MessageType S2C_RAY_CAST = INSTANCE.registerS2C(
@@ -28,11 +26,6 @@ public final class OWLPacketHandler {
 
     public static final MessageType C2S_RAY_CAST = INSTANCE.registerC2S(
             "c2s_ray_cast", ToServerCanSeePos::new);
-
-    public static void sendSyncPresetEntityPacket(JsonPresetEntityHolder<?> holder,
-                                                  ServerLevel level, ChunkPos pos) {
-        new ToClientSyncPresetEntity(holder).sendToChunkListeners(level.getChunk(pos.x, pos.z));
-    }
 
     public static void init() {}
 }
