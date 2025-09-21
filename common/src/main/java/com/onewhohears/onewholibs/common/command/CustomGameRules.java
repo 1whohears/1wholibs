19334252586052fa61941872cc525c7939bf2b3d
1whohears <79810154+1whohears@users.noreply.github.com>
2025-09-21 17:21:29 -0500

    removed test game rules

--- common/src/main/java/com/onewhohears/onewholibs/common/command/CustomGameRules.java
+++ common/src/main/java/com/onewhohears/onewholibs/common/command/CustomGameRules.java
@@ -13,12 +13,12 @@ import java.util.function.BiConsumer;
  */
 public class CustomGameRules {
 
-    public static GameRules.Key<GameRules.BooleanValue> TEST_BOOL;
-    public static GameRules.Key<GameRules.IntegerValue> TEST_INT;
+    //public static GameRules.Key<GameRules.BooleanValue> TEST_BOOL;
+    //public static GameRules.Key<GameRules.IntegerValue> TEST_INT;
 
     public static void register() {
-        TEST_BOOL = registerBoolean("onewholibs:test_bool", false);
-        TEST_INT = registerInteger("onewholibs:test_int", 0, GameRules.Category.MISC);
+        //TEST_BOOL = registerBoolean("onewholibs:test_bool", false);
+        //TEST_INT = registerInteger("onewholibs:test_int", 0, GameRules.Category.MISC);
     }
 
     private static final List<GameRules.Key<GameRules.BooleanValue>> SYNC_BOOLS = new ArrayList<>();
