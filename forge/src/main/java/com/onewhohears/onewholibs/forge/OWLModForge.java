2db9aecf4260833888f1bbae7080458e17174ddb
1whohears <79810154+1whohears@users.noreply.github.com>
2025-09-24 20:20:03 -0500

    changed the facing direction calculations a bit so the shading should be better

--- forge/src/main/java/com/onewhohears/onewholibs/forge/OWLModForge.java
+++ forge/src/main/java/com/onewhohears/onewholibs/forge/OWLModForge.java
@@ -1,13 +1,16 @@
 package com.onewhohears.onewholibs.forge;
 
+import com.mojang.blaze3d.vertex.PoseStack;
 import com.onewhohears.onewholibs.OWLMod;
 import com.onewhohears.onewholibs.client.model.obj.ObjEntityModels;
 import com.onewhohears.onewholibs.client.model.obj.customanims.keyframe.KeyframeAnimsEntityModel;
 import com.onewhohears.onewholibs.client.renderer.RendererObjEntity;
 import com.onewhohears.onewholibs.data.jsonpreset.test.TestPresetGenerator;
+import com.onewhohears.onewholibs.util.math.Vec3f;
 import dev.architectury.platform.Platform;
 import dev.architectury.utils.Env;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.Entity;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.client.event.ModelEvent;
@@ -69,7 +72,14 @@ public final class OWLModForge {
                             ForgeRegistries.ENTITY_TYPES
                     ).get(),
                     context -> new RendererObjEntity<>(context,
-                            new KeyframeAnimsEntityModel<>("ciws_test", "turret_test_anim"))
+                            //new KeyframeAnimsEntityModel<>("ciws_test", "turret_test_anim"))
+                            new KeyframeAnimsEntityModel<Entity>("alexis_plane") {
+                                @Override
+                                protected void rotate(Entity entity, float partialTicks, PoseStack poseStack) {
+                                    super.rotate(entity, partialTicks, poseStack);
+                                    poseStack.mulPose(Vec3f.ZP.rotationDegrees(entity.tickCount).convert());
+                                }
+                            })
             );
         }
         @SubscribeEvent
