cf70f032ca79e109714e3af3cb1a4371a851ac44
1whohears <79810154+1whohears@users.noreply.github.com>
2025-10-15 15:05:40 -0500

    fixed ingredient stack not working in fabric servers

--- common/src/main/java/com/onewhohears/onewholibs/data/crafting/IngredientStack.java
+++ common/src/main/java/com/onewhohears/onewholibs/data/crafting/IngredientStack.java
@@ -61,11 +61,10 @@ public class IngredientStack extends Ingredient {
 				return true;
 		return false;
 	}
-	
-	@Override
-	public ItemStack @NotNull [] getItems() {
-		ItemStack[] items = super.getItems();
-		for (ItemStack item : items) item.setCount(cost);
-		return items;
-	}
+
+    @Override
+    public void dissolve() {
+        super.dissolve();
+        for (ItemStack item : itemStacks) item.setCount(cost);
+    }
 }
