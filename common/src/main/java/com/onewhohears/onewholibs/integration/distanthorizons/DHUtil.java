055f80c8e303f5d0b982d24604ea767b15694777
1whohears <1whohears28@gmail.com>
2025-12-29 14:22:49 -0600

    ALL | remove debug log spam

--- common/src/main/java/com/onewhohears/onewholibs/integration/distanthorizons/DHUtil.java
+++ common/src/main/java/com/onewhohears/onewholibs/integration/distanthorizons/DHUtil.java
@@ -19,18 +19,12 @@ public class DHUtil {
      * {@link com.onewhohears.onewholibs.OWLDependencySafety#distantHorizonsRaycast(Vec3, Vec3, int)}
      */
     public static boolean raycast(Vec3 start, Vec3 end, int maxDistance) {
-        LOGGER.info("Checking DH for a raycast from {} to {}...", start, end);
         Vec3 diff = end.subtract(start);
         maxDistance = Math.min(maxDistance, (int) diff.length());
         DhApiResult<DhApiRaycastResult> result = DhApi.Delayed.terrainRepo.raycast(
                 DhApi.Delayed.worldProxy.getSinglePlayerLevel(),
                 start.x, start.y, start.z, (float) diff.x, (float )diff.y, (float) diff.z,
                 maxDistance, getTerrainCache());
-        DhApiRaycastResult r = result.payload;
-        String dataStr;
-        if (r != null) dataStr = r.dataPoint+"";
-        else dataStr = "null";
-        LOGGER.info("success = {} payload = {}", result.success, dataStr);
         return result.payload == null;
     }
 
