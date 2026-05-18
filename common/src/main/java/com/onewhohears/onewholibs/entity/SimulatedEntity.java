ceb9bde0b0515226af9ecb37866a337db2020f85
1whohears <79810154+1whohears@users.noreply.github.com>
2026-05-18 13:40:28 -0500

    ALL | removed some unused imports

--- common/src/main/java/com/onewhohears/onewholibs/entity/SimulatedEntity.java
+++ common/src/main/java/com/onewhohears/onewholibs/entity/SimulatedEntity.java
@@ -7,7 +7,6 @@ import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerChunkCache;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
